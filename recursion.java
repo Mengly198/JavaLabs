03f39831c43d02749999e8db8dd63298ff74549f
Mengly198 <2023194lay@aupp.edu.kh>
2023-11-23 08:58:05 +0700

    pushed

--- recursion.java
+++ recursion.java
@@ -1,15 +1,23 @@
 public class recursion {
   public static void main(String[] args) {
 
-    //call your recursion fuction
-    //print result
+    System.out.println(reverseString("Hello"));    
+    System.out.println(reverseString1("Hello"));
 
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+  }
+
+  public static String reverseString1(String str) {
+    StringBuilder reversed = new StringBuilder();
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversed.append(str.charAt(i));
     }
-  
+    return reversed.toString();
+  }
+  public static String reverseString(String str) {
+    if (str.isEmpty()) {
+        return str;
+    }
+    return reverseString(str.substring(1)) + str.charAt(0);
 }
-    
+
+}
